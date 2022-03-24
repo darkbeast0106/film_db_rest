9b5714e60bdcc07c74d87dd7ffc62557276aa38f
Budaházi Bence <budahazi.bence@petrik.hu>
2022-03-24 02:46:26 +0100

    Update to Codeigniter 3.1.13

--- system/dotenv/Loader.php
+++ system/dotenv/Loader.php
@@ -128,7 +128,8 @@ class Loader
     }
 
     /**
-     * Read lines from the file, auto detecting line endings.
+     * Read lines from the file
+	 * auto detecting line endings is depreceted as of php 8.1
      *
      * @param string $filePath
      *
@@ -136,12 +137,7 @@ class Loader
      */
     protected function readLinesFromFile($filePath)
     {
-        // Read file into an array of lines with auto-detected line endings
-        //$autodetect = ini_get('auto_detect_line_endings');
-        //ini_set('auto_detect_line_endings', '1');
-        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        //ini_set('auto_detect_line_endings', $autodetect);
-
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);        
         return $lines;
     }
 
