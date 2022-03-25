f47d8cb7d59940325a0039c7af6b1cf4f87a3d9a
Budaházi Bence <budahazi.bence@petrik.hu>
2022-03-25 16:50:37 +0100

    Added react frontend on root.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->helper('url');
+		$this->load->view('film');
+		
+		//$this->load->view('welcome_message');
 	}
 }
---
+++ application/views/film.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta name="theme-color" content="#000000" />
+	<meta name="description" content="Web site created using create-react-app" />
+	<title>React App</title>
+	<script defer="defer" src="<?php echo base_url(); ?>static/js/main.fb5236fe.js"></script>
+	<link href="<?php echo base_url(); ?>static/css/main.04179b6f.css" rel="stylesheet">
+</head>
+
+<body><noscript>You need to enable JavaScript to run this app.</noscript>
+	<div id="root"></div>
+</body>
+</html>
